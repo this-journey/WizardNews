2788a41bb21a489d471d357c88848325c4df9eeb
Demi Zayas <this.journey.dsz@gmail.com>
2023-01-05 20:37:51 -0800

    Added postBank and set up initial server in app

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
+const PORT = 1337;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-const PORT = 1337;
-
 app.listen(PORT, () => {
-  console.log(`App listening in port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
