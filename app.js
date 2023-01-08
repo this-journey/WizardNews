dbf239e1de46658f37c51eef87b97dddf2c88d80
Demi Zayas <this.journey.dsz@gmail.com>
2023-01-07 19:09:35 -0800

    Everything complete other than error handling

--- app.js
+++ app.js
@@ -1,8 +1,82 @@
 const express = require("express");
-const app = express();
-const PORT = 1337;
+const morgan = require("morgan");
+const postBank = require("./postBank");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  const posts = postBank.list();
+  const html = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+        ${posts.map(post => `
+          <div class='news-item'>
+            <p>
+              <span class="news-position">${post.id}. ▲</span>
+              <a href="/posts/${post.id}">${post.title}</a>
+              <small>(by ${post.name})</small>
+            </p>
+            <small class="news-info">
+              ${post.upvotes} upvotes | ${post.date}
+            </small>
+          </div>`
+        ).join('')}
+      </div>
+    </body>
+  </html>`;
+  
+  res.send(html);
+});
+
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  const htmlPost = `<!DOCTYPE html>
+  <html>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <div class="news-list">
+        <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <p>
+              ${post.content}
+            </p>
+          </div>
+      </div>
+    </body>
+  </html>`
+  res.send(htmlPost)
+})
+
+// app.get('/error', (req, res) => {
+//   res.send("404: Page Not Found!")
+// })
+
+// app.use((error, req, res, next) => {
+//   console.error('Error: ', error)
+ 
+//   if (error.type == 'redirect')
+//       res.redirect('/error')
+// })
+
+const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
